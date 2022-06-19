7fb205b7dbd4ea4b907f3094a4efd5f5eb1a4ab4
Julio Cesar de Jesus <cesar.juliojc007@gmail.com>
2022-06-19 12:22:41 -0300

    Adicionado o CheckboxListTile

--- lista_de_tarefas/lib/home_page.dart
+++ lista_de_tarefas/lib/home_page.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-
 import 'models/item.dart';
 
 class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
   List<Item> item = <Item>[];
-  TodoPage() {
+  // HomePage() é um nome reservado do Flutter ?
+  // O HomePage() é um método construtor da pagina, para quando os intems forem
+  //chamados eles apareçam na tela.
+  HomePage() {
     item = [];
     item.add(Item(tarefa: "Compras", done: false));
     item.add(Item(tarefa: "Malhar", done: true));
     item.add(Item(tarefa: "Compras", done: false));
   }
+
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
@@ -27,7 +30,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar,
-      // ListView.bilder() cria uma matriz rolavel na tela criada sob demanda,
+      // ListView.bilder() cria uma matriz rolavel na tela criada sob demanda, não tendo uma lista fixa reinderizada,
       //E para controlar, ele vai precisar do itemCount e o itemBuilder .
       body: Column(
         children: [
@@ -45,14 +48,30 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           Expanded(
-            child: SizedBox(
-              // ListView.bilder() cria uma matriz rolavel na tela criada sob demanda.
-              child: SafeArea(
-                child: ListView.builder(
-                  itemCount: widget.item.length,
-                  itemBuilder: (ctxt, index) {
-                    return Text(widget.item[index].tarefa);
-                  },
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Container(
+                color: Colors.blueGrey,
+                // ListView.bilder() cria uma matriz rolavel na tela criada sob demanda.
+                child: SafeArea(
+                  child: ListView.builder(
+                    // para acessar variaveis e métodos da classe pai, utiliza o 'widget.'
+                    itemCount: widget.item.length,
+                    //o itemBuilder pergunta como construir os widgets na tela.
+                    itemBuilder: (BuildContext ctxt, int index) {
+                      // É criado uma variavel para poder utilizar o widget.item[index] em varios lugares
+                      final item = widget.item[index];
+                      return CheckboxListTile(
+                        title: Text(item.tarefa),
+                        value: item.done,
+                        onChanged: (valor) {
+                          print(valor);
+                        },
+                        //O CheckboxListTile precisa de key que deve ser um item unico
+                        key: Key(item.tarefa),
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
